9c8f7eda30619e1538beb541304bef27e56acf33
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-20 11:39:23 +0000

    add repl support

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #第一次运行解压文件：
-sh 0.sh
+#sh 0.sh
 #启动root
 chmod u+x ./dist/root
 ./dist/root -S . /bin/bash
---
+++ replit.nix
@@ -0,0 +1,7 @@
+{ pkgs }: {
+    deps = [
+        pkgs.bashInteractive
+        pkgs.wget
+        pkgs.unzip
+    ];
+}
